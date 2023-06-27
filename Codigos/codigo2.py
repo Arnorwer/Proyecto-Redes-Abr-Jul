09c6db6d008baa71a82690a07fb52a97b31aca19
Arnorwer <estebanavila322@gmail.com>
2023-06-27 19:36:55 -0400

    codigo2

--- Codigos/codigo2.py
+++ Codigos/codigo2.py
@@ -38,17 +38,53 @@ else: #si la frecuencia no es 60, entonces se calcula manualmente el valor de w 
 
 #listas de valores
 lista_angulos = list()
+lista_V_Xc = list()
+lista_V_Xl = list()
+lista_V_Xr = list()
+lisa_V_Zc = list()
+lista_V_Zl = list()
+lista_V_Zr = list()
 #le damos un valor incial
 lista_angulos = [0]
+lista_V_Xc = [0]
+lista_V_Xl = [0]
+lista_V_Xr = [0]
+lista_V_Zc = [0]
+lista_V_Zl = [0]
+lista_V_Zr = [0]
+
 #Operaciones de los elementos para V_fuente (Hoja2)
 for n in range(1, len(V_fuente["Bus i"])+1):
     #calculo del angulo de desfase
     angulo = round(w * hoja2[3][n])
     #print(angulo)
+    #guardamos el angulo en una lista en orden
     lista_angulos.append(angulo)
+    #Conversiones de mH a H y uF a F
+    converion_Cf = round(hoja2[25][n] * 10 ** -3, 4)
+    conversion_Lf = round(hoja2[5][n] * 10 ** -3, 4)
+    #calculo de la reactancia
+    V_Xl = round(w * conversion_Lf, 4) #reactancia del inductor
+    V_Xc = round(w * converion_Cf, 4) #reactancia del capacitor
+    V_Xc = round(1 / V_Xc, 4) #reactancia del capacitor
+    V_Xr = hoja2[4][n] #reactancia del resistor
+    #guardamos los valores en listas
+    lista_V_Xc.append(V_Xc)
+    lista_V_Xl.append(V_Xl)
+    lista_V_Xr.append(V_Xr)
+    #calculo de impedancia
+    V_Zl = np.complex_(V_Xl * 1j) #impedancia del inductor
+    V_Zc = np.complex_(V_Xc * 1j) #impedancia del capacitor
+    V_Zr = np.complex_(V_Xr * 1j) #impedancia del resistor
+    #guardamos los valores en listas
+    lista_V_Zc.append(V_Zc)
+    lista_V_Zl.append(V_Zl)
+    lista_V_Zr.append(V_Zr)
 
-print(lista_angulos)
-print(lista_angulos[0])
+    #suma de las impedancias de una misma linea desde las listas
+    V
+    
+
 #print(hoja2)
 #calculo para el caso de elementos en un mismo nodo, es decir, paralelo
 #for i in range(len(V_fuente["Bus i"])):
